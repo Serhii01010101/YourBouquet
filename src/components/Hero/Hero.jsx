17a081368cfc85d19cf7fc2e1bc3f0f62a5a7c3b
YaroslavKyba <narxoc.ne@gmail.com>
2024-07-27 13:24:20 +0300

    hero-section

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,5 +1,22 @@
+import styles from './Hero.module.css';
+
 const Hero = () => {
-  return <div>Hero</div>;
+  return (
+    <section className={styles.hero}>
+      <h1 className={styles.heroTitle}>Flowers delivery in Kyiv</h1>
+      <p className={styles.heroText}>
+        Bringing joy to your doorstep with exquisite flower delivery services
+      </p>
+      <div className={styles.heroButtons}>
+        <a href="#contacts" className={styles.heroContactLink}>
+          Contact us
+        </a>
+        <button type="button" className={styles.heroOrderBtn}>
+          Click to order
+        </button>
+      </div>
+    </section>
+  );
 };
 
 export default Hero;
