d497f1be4efbcba2feb8c22a4074156aaf34039a
YaroslavKyba <narxoc.ne@gmail.com>
2024-07-30 15:48:01 +0300

    new icons

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -39,7 +39,7 @@ const Header = () => {
           />
           <button className={styles.searchFormBtn}>
             <svg className={styles.icon}>
-              <use href="../../../public/icons.svg#icon-search"></use>
+              <use href="./../../public/icons.svg#icon-search"></use>
             </svg>
           </button>
         </form>
