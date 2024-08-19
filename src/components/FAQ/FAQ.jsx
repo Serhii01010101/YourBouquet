9a270d4e35dc6a88bc2e8f07f06f52b1e0c9bd48
YaroslavKyba <narxoc.ne@gmail.com>
2024-08-19 09:59:15 +0300

    FAQ icons

--- src/components/FAQ/FAQ.jsx
+++ src/components/FAQ/FAQ.jsx
@@ -1,11 +1,33 @@
-import styles from './FAQ.module.css';
-<script src="../../js/header-button.js"></script>;
+import React, { useState } from 'react';
 
-const FAQ = () => {
+import styles from './FAQ.module.css';
+
+const FAQItem = ({ question, answer }) => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleFAQ = () => {
+    setIsOpen(!isOpen);
+  };
+
   return (
     <section className={styles.FAQ}>
       <h2 className={styles.FAQTitle}>FAQ</h2>
       <ul className={styles.FAQList}>
+        <li className={styles.FAQItem} onClick={toggleFAQ}>
+          <h3 className={styles.FAQItemTitle}>{question}</h3>
+          {isOpen ? (
+            <>
+              <svg className={styles.FAQIcon}>
+                <use href="./../public/icons.svg#icon-minus"></use>
+              </svg>
+              <p className={styles.FAQItemText}>{answer}</p>
+            </>
+          ) : (
+            <svg className={styles.FAQIcon}>
+              <use href="./../public/icons.svg#icon-plus"></use>
+            </svg>
+          )}
+        </li>
         <li className={styles.FAQItem}>
           <h3 className={styles.FAQItemTitle}>
             What types of flowers can I order on your website?
@@ -16,7 +38,7 @@ const FAQ = () => {
             classic bouquets and exclusive arrangements to suit any taste.
           </p>
           <svg className={styles.FAQIcon}>
-            <use href="/../../public/icons.svg#icon-plus"></use>
+            <use href="../../../public/icons.svg#icon-plus"></use>
           </svg>
         </li>
         <li className={styles.FAQItem}>
@@ -29,20 +51,7 @@ const FAQ = () => {
             classic bouquets and exclusive arrangements to suit any taste.
           </p>
           <svg className={styles.FAQIcon}>
-            <use href="/../../public/icons.svg#icon-plus"></use>
-          </svg>
-        </li>
-        <li className={styles.FAQItem}>
-          <h3 className={styles.FAQItemTitle}>
-            What types of flowers can I order on your website?
-          </h3>
-          <p className={styles.FAQItemText}>
-            On our website, you can order a variety of flowers, including roses,
-            tulips, orchids, lilies, chrysanthemums, and more. We offer both
-            classic bouquets and exclusive arrangements to suit any taste.
-          </p>
-          <svg className={styles.FAQIcon}>
-            <use href="/../../public/icons.svg#icon-plus"></use>
+            <use href="./public/icons.svg#icon-plus"></use>
           </svg>
         </li>
       </ul>
@@ -50,4 +59,28 @@ const FAQ = () => {
   );
 };
 
-export default FAQ;
+const FAQList = () => {
+  const faqs = [
+    {
+      index: 1,
+      question: 'What types of flowers can I order on your website?',
+      answer:
+        'On our website, you can order a variety of flowers, including roses, tulips, orchids, lilies, chrysanthemums, and more. We offer both classic bouquets and exclusive arrangements to suit any taste.',
+    },
+  ];
+
+  return (
+    <div className="faq-container">
+      <h2>FAQ</h2>
+      {faqs.map((faq) => (
+        <FAQItem
+          key={faq.indexndex}
+          question={faq.question}
+          answer={faq.answer}
+        />
+      ))}
+    </div>
+  );
+};
+
+export default FAQList;
