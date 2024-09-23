37f5b436209c2d92edaa422949637ce85ba8f2d3
YaroslavKyba <narxoc.ne@gmail.com>
2024-09-23 12:58:11 +0300

    icon

--- src/components/ChooseUs/ChooseUs.jsx
+++ src/components/ChooseUs/ChooseUs.jsx
@@ -1,5 +1,10 @@
 import styles from './ChooseUs.module.css';
 
+import pathSpriteEasyOrder from '../../../public/icons.svg#icon-easy-order';
+import pathSpriteFastDelivery from '../../../public/icons.svg#icon-fast-delivery';
+import pathSpritePremiumQuality from '../../../public/icons.svg#icon-premium-quality';
+import pathSpriteOwnDesign from '../../../public/icons.svg#icon-your-own-design';
+
 const ChooseUs = () => {
   return (
     <section className={styles.chooseUs}>
@@ -10,25 +15,25 @@ const ChooseUs = () => {
         <ul className={styles.chooseUsList}>
           <li className={styles.chooseUsItem}>
             <svg className={styles.chooseUsItemIcon}>
-              <use href="../../../public/icons.svg#icon-easy-order"></use>
+              <use href={pathSpriteEasyOrder}></use>
             </svg>
             <h3 className={styles.chooseUsItemTitle}>Easy order</h3>
           </li>
           <li className={styles.chooseUsItem}>
             <svg className={styles.chooseUsItemIcon}>
-              <use href="../../../public/icons.svg#icon-fast-delivery"></use>
+              <use href={pathSpriteFastDelivery}></use>
             </svg>
             <h3 className={styles.chooseUsItemTitle}>Fast delivery</h3>
           </li>
           <li className={styles.chooseUsItem}>
             <svg className={styles.chooseUsItemIcon}>
-              <use href="../../../public/icons.svg#icon-premium-quality"></use>
+              <use href={pathSpritePremiumQuality}></use>
             </svg>
             <h3 className={styles.chooseUsItemTitle}>Premium Quality</h3>
           </li>
           <li className={styles.chooseUsItem}>
             <svg className={styles.chooseUsItemIcon}>
-              <use href="../../../public/icons.svg#icon-your-own-design"></use>
+              <use href={pathSpriteOwnDesign}></use>
             </svg>
             <h3 className={styles.chooseUsItemTitle}>Your own design</h3>
           </li>
